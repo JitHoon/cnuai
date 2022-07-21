8ef7bd64bfe289aac6e4fb182cfb35354a6cbd6f
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-07-21 11:57:46 +0900

    07.21.morning django

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 # REST API 사용을 위해서 jsonify, request module을 불러온다.
 # jsonify :  js 데이터 저장 방식으로 변환해주는 module
 # request : HTTP module을 다룰 수 있는 module
+# ex) POST로 자원이 추가될 때 그 내용을 담고 있음
 
 # flask의 활성 모듈의 이름 __name__ 을 app으로 지정함
 app = Flask(__name__)
@@ -30,15 +31,18 @@ def get_myName():
     # 리스트는 json으로 변환될 수 없으므로 dic꼴로 바꾸고 jsonify module 사용
     return jsonify({"myName": myName})
 
-# POST /myName | 자료를 /myName 자원에 추가한다
+# POST /myName | 자료를 /myName 자원에 추가한다.
 
 
 @app.route('/myName', methods=['POST'])
-def create_menu():
-    # 전달받은 자료를 menus 자원에 추가
+def create_myName():
+    # 전달받은 자료를 myName 자원에 추가
+    # request에 담겨있는 POST 요청 정보를 get_json()으로 json 형태로 받아온다.
     request_data = request.get_json()
+    # POST 요청시 newName 정보를 request.get_json()으로 받아옴
     newName = {
-        "id": 4, "name": request_data['name']
+        "id": 4,
+        "name": request_data['name']
     }
     myName.append(newName)
     return jsonify(newName)
