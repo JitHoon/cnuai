8d12e9d23180fe235c4500b31ef8bb41785804d6
JitHoon <101972330+JitHoon@users.noreply.github.com>
2022-07-21 13:44:17 +0900

    07.21.afternoon django views, urls, setting

--- django/webproject/webprojectApp/views.py
+++ django/webproject/webprojectApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+# request가 들어오면 HTTPResponse()를 return
+
+
+def index(request):
+    return HttpResponse("Hello this is index App! (HTTP Response by django from webprogectApp/views.py)")
